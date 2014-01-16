54f24846fe55b6bdeecc776004622d114bcb15cd
Chris Welty <chris.welty.1@gmail.com>
2014-01-16 15:11:54 -0600

    Works on Mac

--- src/core/Debug.h
+++ src/core/Debug.h
@@ -3,20 +3,7 @@
 // in replacement of crtdbg.h
 ///////////////////////////////////////////////////
 
-#ifndef H_DEBUG
-#define H_DEBUG
+#pragma once
 
-#ifdef _WIN32
-#include <crtdbg.h>
-#endif
-
-#ifdef __unix__
-
-#ifndef _ASSERT
 #include <cassert>
 #define _ASSERT(x) assert(x)
-#endif
-
-#endif //__unix__
-
-#endif //H_DEBUG
--- src/core/Ticks.cpp
+++ src/core/Ticks.cpp
@@ -26,26 +26,12 @@ i8 GetTicksPerSecond(void) {
 	return (i8)(ticks.QuadPart);
 }
 
-#elif defined(__unix__)
+#else
 
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <fcntl.h>
 #include <unistd.h>
-
-#if 0 //USE CPU elapsed time
-
-i8 GetTicks(void) {
-	struct rusage u;
-	getrusage(RUSAGE_SELF, &u);
-	return (i8) 1000000LL * u.ru_utime.tv_sec + u.ru_utime.tv_usec;	
-}
-
-i8 GetTicksPerSecond(void) {
-	return 1000000LL;
-}
-
-#else //use real time (ie clock on the wall)
 
 i8 GetTicks(void) {
 	struct timeval tv;
@@ -57,16 +43,4 @@ i8 GetTicksPerSecond(void) {
 	return (i8) 1000000;
 }
 
-#endif // real time
-
-#else // ANSI
-
-i8 GetTicks(void) {
-	return (i8) clock();
-}
-
-i8 GetTicksPerSecond(void) {
-	return (i8) CLOCKS_PER_SEC;
-}
-
-#endif // ANSI
+#endif // _WIN32
