f7790cd4ac6fd2fb5a3e81af51d81dabf8e900d8
Vlad Petric <vlad@petric.cc>
2014-06-15 17:43:10 -0700

    Even more fixes for disp.py

--- extras/dis/disp.py
+++ extras/dis/disp.py
@@ -60,13 +60,20 @@ def ProcessFuncLines(func, lines, show_dis):
                             loads += 1
                     elif p2.addr is not None:
                         assert p1.addr is None
-                        total_count += 2
-                        if p2.addr.is_stack_based():
-                            stack_stores += 1
-                            stack_loads += 1
+                        if insn.opcode.startswith("mov"):
+                            total_count += 1
+                            if p2.addr.is_stack_based():
+                                stack_stores += 1
+                            else:
+                                stores += 1
                         else:
-                            stores += 1
-                            loads += 1
+                            total_count += 2
+                            if p2.addr.is_stack_based():
+                                stack_stores += 1
+                                stack_loads += 1
+                            else:
+                                stores += 1
+                                loads += 1
             elif insn.opcode.startswith("mov"):
                 assert len(insn.params) == 2
                 p1 = insn.params[0] 
@@ -127,8 +134,12 @@ def ProcessFuncLines(func, lines, show_dis):
     if len(unhandled_opcodes) > 0:
         print("WARNING: there are unhandled opcodes:", " ".join(unhandled_opcodes), file=sys.stderr)
 
-def HandleFunction(filename, funcname, show_dis):
-    dis_subproc = subprocess.Popen(DISASSEMBLER + [filename], stdout = subprocess.PIPE, stderr = None)
+def HandleFunction(filename, funcname, show_dis, ovrdis):
+    if ovrdis is None: 
+        arglist = DISASSEMBLER + [filename]
+    else:
+        arglist = [ovrdis] + DISASSEMBLER[1:] + [filename]
+    dis_subproc = subprocess.Popen(arglist, stdout = subprocess.PIPE, stderr = None)
     last_func = None
     good_lines = []
 
@@ -158,9 +169,10 @@ def main():
     parser = argparse.ArgumentParser("Disassembly inspector")
     parser.add_argument("filename")
     parser.add_argument("funcname")
-    parser.add_argument("-d", "--dis", action="store_true", default=False)
+    parser.add_argument("-d", "--dis", action="store_true", default=False, help="Show disassembly")
+    parser.add_argument("--ovrdis", action="store", default=None, help="Override disassembly tool (default is objdump)")
     args = parser.parse_args()
-    HandleFunction(args.filename, args.funcname, args.dis)
+    HandleFunction(args.filename, args.funcname, args.dis, args.ovrdis)
 
 if __name__ == "__main__":
     main()
